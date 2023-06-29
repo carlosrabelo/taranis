a34af8433d866e8e7e13357059437eabb922ca50
Carlos Rabelo <contato@carlosrabelo.com.br>
2023-06-29 14:33:49 -0400

    Implementing the registers

--- z80/z80.go
+++ z80/z80.go
@@ -1,12 +1,37 @@
 package z80
 
 type CPU struct {
+	GPRc
+	GPRa
 	SPR
+}
+
+type GPRc struct {
+        A uint8
+        F uint8
+        B uint8
+        C uint8
+        D uint8
+        E uint8
+        H uint8
+        L uint8
+}
+
+type GPRa struct {
+        A_ uint8
+        F_ uint8
+        B_ uint8
+        C_ uint8
+        D_ uint8
+        E_ uint8
+        H_ uint8
+        L_ uint8
 }
 
 type SPR struct {
 	I uint8
         R uint8
+
 	IX uint16
 	IY uint16
 	SP uint16
